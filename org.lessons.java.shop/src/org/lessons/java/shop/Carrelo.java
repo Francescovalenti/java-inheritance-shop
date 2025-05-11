56e5183ac3a33cbdf27bdd0461c337f09351006e
francescovalenti <francescovalenti83@outlook.it>
2025-05-11 10:20:57 +0200

    feat:inizio

---
+++ org.lessons.java.shop/src/org/lessons/java/shop/Carrelo.java
@@ -0,0 +1,9 @@
+package org.lessons.java.shop;
+import java.util.Scanner;
+
+public class Carrelo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    
+}
+}
