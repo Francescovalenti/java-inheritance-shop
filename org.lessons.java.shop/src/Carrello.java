c943ecfd17a51016ac75e240d445c198048522cd
francescovalenti <francescovalenti83@outlook.it>
2025-05-11 19:59:05 +0200

    feat:inizio con le array

--- org.lessons.java.shop/src/Carrello.java
+++ org.lessons.java.shop/src/Carrello.java
@@ -1,9 +1,11 @@
-package org.lessons.java.shop;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Carrelo {
+public class Carrello {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Prodotto> carrello=new ArrayList<>(3);
     
 }
 }
