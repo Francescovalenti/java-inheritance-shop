9b69329a55e474af793360a7db0fb65458285697
francescovalenti <francescovalenti83@outlook.it>
2025-05-13 11:43:06 +0200

    feat:aggiunta con il metodo switch

--- org.lessons.java.shop/src/Carrello.java
+++ org.lessons.java.shop/src/Carrello.java
@@ -9,49 +9,52 @@ public class Carrello {
         for (int i = 0; i < Carrello.length; i++) {
             // inserimento della prima domanda
             System.out.println("Inserisci la tipologia del prodotto acquistato:");
-            String TipoProdotto = scanner.nextLine().trim();
-            // inserimento dei vari prodotti e delle loro scelte.
-            if (TipoProdotto.equalsIgnoreCase("Smartphone")) {
-                System.out.println("Nome:");
-                String Nome = scanner.nextLine().trim();
-                System.out.println("Marca:");
-                String Marca = scanner.nextLine().trim();
+            String TipoProdotto = scanner.nextLine();
+            
+           
+            // inserimento con il metodo switch per la scelta del cliente
+            switch (TipoProdotto.trim().toLowerCase()) {
+                case "smartphone":{
+                    System.out.println("Nome:");
+                    String Nome = scanner.nextLine().trim();
+                    System.out.println("Marca:");
+                    String Marca = scanner.nextLine().trim();
 
-                Carrello[i] = new Smartphone("Galaxy S24", "Samsung", 0, 500, 22, "b0123456789", 128);
+                    Carrello[i] = new Smartphone("Galaxy S24", "Samsung", 0, 500, 22, "b0123456789", 128);
+                    break;
+                }
+                case "televisori":
+                    System.out.println("Nome:");{
+                    String Nome = scanner.nextLine().trim();
 
+                    System.out.println("Marca:");
+                    String Marca = scanner.nextLine().trim();
+
+                    Carrello[i] = new Televisori("Bravia X90", "Sony", 0, 1499, 22, 55, true);
+                    break;}
+                case "cuffie":{
+                    System.out.println("Nome:");
+                    String Nome = scanner.nextLine().trim();
+
+                    System.out.println("Marca:");
+                    String Marca = scanner.nextLine().trim();
+
+                    Carrello[i] = new Cuffie("AirPods", "Apple", 0, 199, 22, "Bianco", "wireless");}
+// inserimento per la risposta sbagliata del cliente
+                default:
+                    System.out.println("Tipologia non riconosciuta. Salto questo prodotto o ritorna indietro.");
+                    break;
             }
+            
 
-            else if (TipoProdotto.equalsIgnoreCase("Televisori")) {
-                System.out.println("Nome:");
-                String Nome = scanner.nextLine().trim();
-                System.out.println("Marca:");
-                String Marca = scanner.nextLine().trim();
-
-                Carrello[i] = new Televisori("Bravia X90", "Sony", 0, 1499, 22, 55, true);
-            } else if (TipoProdotto.equalsIgnoreCase("Cuffie")) {
-                System.out.println("Nome:");
-                String Nome = scanner.nextLine().trim();
-                System.out.println("Marca:");
-                String Marca = scanner.nextLine().trim();
-
-                Carrello[i] = new Cuffie("AirPods", "Apple", 0, 199, 22, "Bianco", "wireless");
-                // inserimento della risposta per possibile errore del cliente
-            } else {
-                System.out.println("Tipologia non riconosciuta. Salto questo prodotto o ritorna indietro.");
-            }
-
-         
-
-  
-            System.out.println("\n--- CONTENUTO DEL CARRELLO ---");
+            System.out.println("--- CONTENUTO DEL CARRELLO ---");
             for (Prodotto Finale : Carrello) {
                 if (Finale != null)
                     System.out.println(Finale);
-            }
-        
 
-      
+            }
+
+        }
+        scanner.close();
     }
-      scanner.close();
-}
 }
